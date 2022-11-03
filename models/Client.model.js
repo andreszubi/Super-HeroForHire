0998e28d654cee210d4bf7a7beb8e9fe216b375c
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2022-11-03 17:50:45 +0100

    merge conflict

---
+++ models/Client.model.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const clientSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    postalcode: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: Number,
+      required: true
+    },
+
+  }
+);
+
+const Client = model("Client", clientSchema);
+
+module.exports = Client;
---
+++ models/Professional.model.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const proSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    postalcode: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    services: {
+        type: String,
+        required: true
+    },
+    specialties: {
+        type: String,
+        required: true
+    }
+
+  }
+);
+
+const Professional = model("Professional", proSchema);
+
+module.exports = Professional;
