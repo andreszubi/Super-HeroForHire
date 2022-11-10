074fb1f0f839dc84bab14e782bd41d10f0f82ed7
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2022-11-10 17:20:09 +0100

    deleted unnecessary files and commented out code

--- routes/pro-logged.routes.js
+++ routes/pro-logged.routes.js
@@ -73,53 +73,7 @@ router.post(
   }
 );
 
-//   if (email === "" || password === "") {
-//     res.render("auth/pro-signup", {
-//       errorMessage:
-//         "All fields are mandatory. Please provide your email and password.",
-//     });
-//     return;
-//   }
-//   const emailRegex = /@/;
-//   if (!emailRegex.test(email)) {
-//     res.render("auth/pro-signup", {
-//       errorMessage: "Email format is not valid.",
-//     });
-//     return;
-//   }
-//   const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-//   if (!passwordRegex.test(password)) {
-//     res.render("auth/pro-signup", {
-//       errorMessage:
-//         "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
-//     });
-//     return;
-//   }
-//   if (services.length === 0) {
-//     res.render("auth/pro-signup", {
-//       errorMessage: "You must select at least one service.",
-//     });
-//     return;
-//   }
 
-//   if (price < 0) {
-//     res.render("auth/pro-signup", {
-//       errorMessage: "Price per hour must be filled out.",
-//     });
-//     return;
-//   }
-
-// if (error instanceof mongoose.Error.ValidationError) {
-//   res
-//     .status(500)
-//     .render("auth/pro-signup", { errorMessage: error.message });
-// } else if (error.code === 11000) {
-//   res.status(500).render("auth/pro-signup", {
-//     errorMessage: "Email needs to be unique. The email is already used.",
-//   });
-// } else {
-//   next(error);
-// }
 
 //GET/POST route for Login
 
